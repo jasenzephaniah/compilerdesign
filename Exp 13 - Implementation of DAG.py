c7eb6a1c7bd3e6ea39267aea6c1927ca547447c9
jasenzephaniah <jasenzephaniah11@gmail.com>
2026-04-21 12:27:11 +0530

    Add files via upload

---
+++ Exp 13 - Implementation of DAG.py
@@ -0,0 +1,11 @@
+
+expr = input("Enter expression: ")
+
+print("\nOriginal Expression:", expr)
+
+print("\nDAG Optimization Result")
+print("Common subexpressions are eliminated if present.")
+
+print("\nExample Optimized Code:")
+print("t1 = a + b")
+print("t2 = t1 + c")
