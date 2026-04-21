2104f1a75a04dcee788a568176f035873a09bc7e
jasenzephaniah <jasenzephaniah11@gmail.com>
2026-04-21 12:24:45 +0530

    Add files via upload

---
+++ Exp 11 - quadruple triple indirect triple.py
@@ -0,0 +1,22 @@
+
+
+
+op = input("Enter Operator: ")
+arg1 = input("Enter Argument 1: ")
+arg2 = input("Enter Argument 2: ")
+result = input("Enter Result Variable: ")
+
+
+print("\nQUADRUPLE")
+print("Op\tArg1\tArg2\tResult")
+print(f"{op}\t{arg1}\t{arg2}\t{result}")
+
+
+print("\nTRIPLE")
+print("Index\tOp\tArg1\tArg2")
+print(f"0\t{op}\t{arg1}\t{arg2}")
+
+
+print("\nINDIRECT TRIPLE")
+print("Pointer Table")
+print("0 -> (0)")
